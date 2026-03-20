3276575aaeb196592e83c5cf296ec22315ec2c60
jjaemun <jjaemuuu@gmail.com>
2026-03-20 22:39:15 +0100

    conversions

--- include/date/conversions.hpp
+++ include/date/conversions.hpp
@@ -9,33 +9,34 @@ namespace tts {
     
     // copy-to-conversions.
     [[nodiscard]]
-    constexpr i32 to_unix_date(Date date) noexcept {
+    constexpr i32 to_unix_date(Date date) {
         return  date.value;
     }
 
     [[nodiscard]]
-    constexpr i32 to_excel_date(Date date) noexcept {
+    constexpr i32 to_excel_date(Date date) {
         return  date.value + excel::date::OFFSET;
     }
 
     [[nodiscard]]
-    constexpr i32 to_julian_date(Date date) noexcept {
+    constexpr i32 to_julian_date(Date date) {
         return  date.value + julian::date::OFFSET;
     }
  
     // copy-from-conversions.
     [[nodiscard]]
-    constexpr Date from_unix_date(i32 serial) noexcept {
+    constexpr Date from_unix_date(i32 serial) {
+        assert_valid(serial, unix::date::MIN, unix::date::MAX);
         return Date{serial};
     }
 
     [[nodiscard]]
-    constexpr Date from_excel_date(i32 serial) noexcept {
+    constexpr Date from_excel_date(i32 serial) {
         return Date{serial - excel::date::OFFSET};
     }
 
     [[nodiscard]]
-    constexpr Date from_julian_date(i32 serial) noexcept {
+    constexpr Date from_julian_date(i32 serial) {
         return Date{serial - julian::date::OFFSET};
     }
 
