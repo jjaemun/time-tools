7d8e0d69cc44a583cccc41a53aa47067df733696
jjaemun <jjaemuuu@gmail.com>
2026-03-20 16:09:25 +0100

    date

--- include/date/date.hpp
+++ include/date/date.hpp
@@ -8,6 +8,11 @@ namespace tts {
     class Date final {
         public:
             i32 value;
+
+        // copy-accessors!
+        [[nodiscard]] 
+        inline constexpr i32 to_serial_date(void) const {
+            return value;
+        }
     };
 } // namespace tts.
-    
