ed2ef961736b73aca50b31412371cb9627572e9d
jjaemun <jjaemuuu@gmail.com>
2026-03-20 22:49:03 +0100

    assert

---
+++ include/assert.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+
+#include <cassert>
+
+
+namespace tts {
+    // assertions!
+    template <typename T>
+    constexpr void assert_eq(T a, T b) noexcept {
+        return assert(a == b);
+    }
+         
+    template <typename T>
+    constexpr void assert_neq(T a, T b) noexcept {
+        return assert(a != b);
+    }
+
+    template <typename T>
+    constexpr void assert_lt(T a, T b) noexcept {
+        return assert(a < b);
+    }
+    
+    template <typename T>
+    constexpr void assert_le(T a, T b) noexcept {
+        return assert(a <= b);
+    }
+    
+    template <typename T>
+    constexpr void assert_gt(T a, T b) noexcept {
+        return assert(a > b);
+    }
+    
+    template <typename T>
+    constexpr void assert_ge(T a, T b) noexcept {
+        return assert(a <= b);
+    }
+} // namespace tts.
