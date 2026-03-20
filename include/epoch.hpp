e86108546b918c6fe1fb38fee64e3d4b61206628
jjaemun <jjaemuuu@gmail.com>
2026-03-20 20:12:58 +0100

    epoch

---
+++ include/epoch.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+
+#include "types.hpp"
+
+
+namespace tts {
+
+    /**
+     *
+    */
+
+    // types.
+    enum class epoch {
+        unix,
+        excel,
+        murex,
+        julian
+    };
+} // namespace tts.
