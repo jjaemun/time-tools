d4a70a529286c9d4ab669824d334883440591013
ehrlz <ehrlz@proton.me>
2026-03-21 00:38:27 +0100

    murex const

--- include/date/constants.hpp
+++ include/date/constants.hpp
@@ -14,23 +14,25 @@ namespace tts {
 
     // constants.
     namespace unix::date {
-        constexpr i32 OFFSET{0};
+        constexpr i32 OFFSET{0}; // 1970-1-1
         constexpr i32 MIN{3652};
         constexpr i32 MAX{84370};
     }
 
     namespace excel::date {
-        constexpr i32 OFFSET{25569};
+        constexpr i32 OFFSET{-25569}; // 1899-12-30
         constexpr i32 MIN{OFFSET + unix::date::MIN};
         constexpr i32 MAX{OFFSET + unix::date::MAX};     
     }
     
     namespace murex::date {
-        //TODO.
+        constexpr i32 OFFSET{3652}; // 1980-1-1
+        constexpr i32 MIN{OFFSET + unix::date::MIN};
+        constexpr i32 MAX{OFFSET + unix::date::MAX};     
     }
  
     namespace julian::date {
-        constexpr i32 OFFSET{2440588};
+        constexpr i32 OFFSET{-2440588}; // 4713-1-1 b.C. julian / 4714-11-24 b.C. gregorian
         constexpr i32 MIN{OFFSET + unix::date::MIN};
         constexpr i32 MAX{OFFSET + unix::date::MAX};
     }
