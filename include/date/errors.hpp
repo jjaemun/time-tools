91a06b3bd02168a5403c57aaabd7e974e1361e28
jjaemun <jjaemuuu@gmail.com>
2026-03-21 12:35:04 +0100

    descriptors

--- include/date/errors.hpp
+++ include/date/errors.hpp
@@ -1,8 +1,31 @@
 #pragma once
 
 
+#include <string_view>
+#include <utility>
+
+
 namespace tts::err {
+    // types!
     enum class date {
         invalid
     };
+
+    // descriptors!
+    inline std::string_view to_str(date code) {
+
+        /**
+         * Converts date submodule error code to str view.
+        */
+
+        switch (code) {
+            case (date::invalid):
+                return "Date outside valid interval.";
+            
+            default:
+                std::unreachable();
+        }
+    }
 } // namespace tts::err.
+
+
