248d4a9254dbb8f03bb6df71868db936f8d6e7e0
jjaemun <jjaemuuu@gmail.com>
2026-03-21 11:41:35 +0100

    cosmetic...

--- include/date/constants.hpp
+++ include/date/constants.hpp
@@ -9,36 +9,35 @@ namespace tts {
     /**
      * Internally tts uses unix epoch convention for serials, and so all dates
      * are expressed relative to the origin 1970-01-01. Additionally, dates are
-     * restricted to the inclusive interval between 1980-01-01 and 2200-12-31.
+     * restricted to the (inclusive) interval between 1980-01-01 and 2200-12-31.
      */
 
     // constants.
     namespace unix::date {
-        // epoch! 1970-01-01.
+        // epoch 1970-01-01.
         constexpr i32 OFFSET{0};
         constexpr i32 MIN{3652};
         constexpr i32 MAX{84370};
-    }
+    } // namespace unix::date.
 
     namespace excel::date {
-        // epoch! 1899-12-30.
+        // epoch 1899-12-30.
         constexpr i32 OFFSET{25569};
         constexpr i32 MIN{OFFSET + unix::date::MIN};
         constexpr i32 MAX{OFFSET + unix::date::MAX};     
-    }
+    } // namespace excel::date.
     
     namespace murex::date {
-        // epoch! 1980-01-01.
+        // epoch 1980-01-01.
         constexpr i32 OFFSET{3652};
         constexpr i32 MIN{OFFSET + unix::date::MIN};
         constexpr i32 MAX{OFFSET + unix::date::MAX};     
-    }
+    } // namespace murex::date.
 
     namespace julian::date {
-        // epoch! (b.C.) 4714-11-24.
+        // epoch (b.C.) 4714-11-24.
         constexpr i32 OFFSET{2440588};
         constexpr i32 MIN{OFFSET + unix::date::MIN};
         constexpr i32 MAX{OFFSET + unix::date::MAX};
-    }
-
+    } // namespace julian::date.
 } // namespace tts.
