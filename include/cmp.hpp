a6fe31d3788f971b446b9d74d8d9ec3cb533ccdc
jjaemun <jjaemuuu@gmail.com>
2026-03-21 13:49:56 +0100

    cmp

--- include/cmp.hpp
+++ include/cmp.hpp
@@ -3,39 +3,39 @@
 
 namespace tts {
     // cmp!
-    [[nodiscard]]
     template <typename T>
+    [[nodiscard]]
     constexpr bool cmpeq(T a, T b) noexcept {
         return (a == b);
     }
          
-    [[nodiscard]]
     template <typename T>
+    [[nodiscard]]
     constexpr bool cmpneq(T a, T b) noexcept {
         return (a != b);
     }
 
-    [[nodiscard]]
     template <typename T>
+    [[nodiscard]]
     constexpr bool cmplt(T a, T b) noexcept {
         return (a < b);
     }
     
-    [[nodiscard]]
     template <typename T>
+    [[nodiscard]]
     constexpr bool cmple(T a, T b) noexcept {
         return (a <= b);
     }
     
-    [[nodiscard]]
     template <typename T>
+    [[nodiscard]]
     constexpr bool cmpgt(T a, T b) noexcept {
         return (a > b);
     }
     
-    [[nodiscard]]
     template <typename T>
+    [[nodiscard]]
     constexpr bool cmpge(T a, T b) noexcept {
-        return assert(a <= b);
+        return assert(a >= b);
     }
 } // namespace tts.
