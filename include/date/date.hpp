3c227cfb0f27909021008bbee50cc0c210ff8201
jjaemun <jjaemuuu@gmail.com>
2026-03-21 13:44:35 +0100

    date

--- include/date/date.hpp
+++ include/date/date.hpp
@@ -17,30 +17,35 @@ namespace tts {
 
         // copy-accessors!
         [[nodiscard]] 
-        constexpr i32 to_unix_serial(void) const {
+        constexpr i32 to_unix_serial(void) const noexcept {
             return value;
         }
 
         [[nodiscard]] 
-        constexpr i32 to_excel_serial(void) const {
+        constexpr i32 to_excel_serial(void) const noexcept {
             return value + excel::date::OFFSET;
         }
 
         [[nodiscard]] 
-        constexpr i32 to_murex_serial(void) const {
+        constexpr i32 to_murex_serial(void) const noexcept {
             return value + murex::date::OFFSET;
         }
         
         [[nodiscard]] 
-        constexpr i32 to_julian_serial(void) const {
+        constexpr i32 to_julian_serial(void) const noexcept {
             return value + julian::date::OFFSET;
         }
 
-        // mutable-ref-accessors!
-        [[nodiscard]] 
-        constexpr i32 &as_unix_serial(void) {
+        // immutable-ref-accessor!
+        [[nodiscard]]
+        constexpr const i32 &as_unix_serial(void) const noexcept {
+            return value;
+        }
+
+        // mutable-ref-accessor!
+        constexpr i32 &as_mut_unix_serial(void) noexcept {
             return value;
         }       
-
+  
     };
 } // namespace tts.
