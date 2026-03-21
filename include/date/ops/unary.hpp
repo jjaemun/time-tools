13577ae5d3b6270c56252f96a5b414d427b9c3a3
jjaemun <jjaemuuu@gmail.com>
2026-03-21 16:37:51 +0100

    errors

--- include/date/ops/unary.hpp
+++ include/date/ops/unary.hpp
@@ -1,39 +1,50 @@
 #pragma once 
 
 
-#include "assert.hpp"
+#include "cmp.hpp"
 #include "date/constants.hpp"
+#include "date/exceptions.hpp"
 #include "date/date.hpp"
 
 
 namespace tts {
     // prefix!
-    inline Date &operator++(Date &date) noexcept {
-        assert_lt(date.value, unix::date::MAX);
+    inline Date &operator++(Date &date) {
+        if (!cmplt(date.value, unix::date::MAX))
+            throw DateError(err::date::overflow);
+
         ++date.value;
         return date;
     }
     
-    inline Date &operator--(Date &date) noexcept {
-        assert_gt(date.value, unix::date::MIN);
+    inline Date &operator--(Date &date) {
+        if (!cmpgt(date.value, unix::date::MIN))
+            throw DateError(err::date::underflow);
+
         --date.value;
         return date;
     }
 
     //postfix!
     [[nodiscard]]
-    inline Date operator++(Date &date, int) noexcept {
-        assert_lt(date.value, unix::date::MAX);
+    inline Date operator++(Date &date, int) {
+        if (!cmplt(date.value, unix::date::MAX))
+            throw DateError(err::date::overflow);
+
         Date deepcopy{date};
         ++date.value;
+
         return deepcopy;
     }
 
     [[nodiscard]]
-    inline Date operator--(Date &date, int) noexcept {
-        assert_gt(date.value, unix::date::MIN);
+    inline Date operator--(Date &date, int) {
+        if (!cmpgt(date.value, unix::date::MIN))
+            throw DateError(err::date::underflow);
+
         Date deepcopy{date};
         --date.value;
+
         return deepcopy;
     }
 } //namespace tts.
