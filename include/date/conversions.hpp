41873e71815fea9f85b74695458f49dc185d756e
jjaemun <jjaemuuu@gmail.com>
2026-03-21 13:04:57 +0100

    conversions

--- include/date/conversions.hpp
+++ include/date/conversions.hpp
@@ -31,32 +31,32 @@ namespace tts {
     // copy-from-conversions.
     [[nodiscard]]
     constexpr Date from_unix_date(i32 serial) {
-        if !(unix::date::is_valid(serial))
-            throw DateError(err::date::invalid));
+        if (!unix::date::is_valid(serial))
+            throw DateError(err::date::invalid);
 
         return Date{serial};
     }
 
     [[nodiscard]]
     constexpr Date from_excel_date(i32 serial) {
-        if !(excel::date::is_valid(serial))
-            throw DateError(err::date::invalid));
+        if (!excel::date::is_valid(serial))
+            throw DateError(err::date::invalid);
 
         return Date{serial - excel::date::OFFSET};
     }
 
     [[nodiscard]]
     constexpr Date from_murex_date(i32 serial) {
-        if !(murex::date::is_valid(serial))
-            throw DateError(err::date::invalid));
+        if (!murex::date::is_valid(serial))
+            throw DateError(err::date::invalid);
 
-        return Date{serial - excel::date::OFFSET};
+        return Date{serial - murex::date::OFFSET};
     }
 
     [[nodiscard]]
     constexpr Date from_julian_date(i32 serial) {
-        if !(julian::date::is_valid(serial))
-            throw DateError(err::date::invalid));
+        if (!julian::date::is_valid(serial))
+            throw DateError(err::date::invalid);
 
         return Date{serial - julian::date::OFFSET};
     }
