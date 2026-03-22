22e74430c912e5d21e48181ae5fff93afd63b6ca
jjaemun <jjaemuuu@gmail.com>
2026-03-22 12:49:55 +0100

    civil

--- include/date/gregorian/civil.hpp
+++ include/date/gregorian/civil.hpp
@@ -2,6 +2,7 @@
 
 
 #include "types.hpp"
+#include "cmp.hpp"
 
 
 namespace tts {
@@ -15,35 +16,35 @@ namespace tts {
     public:
         i32 year;
         u16 month;
-        u16 day;a
+        u16 day;
 
         // attr!
         [[nodiscard]]
-        constexpr bool days_in_month() const noexcept {
+        constexpr u16 days_in_month() const noexcept {
             switch (month) {
                 case (1):
                     // january.
-                    [[fallthrough]]
+                    [[fallthrough]];
 
                 case (3):  
                     // march.
-                    [[fallthrough]]
+                    [[fallthrough]];
 
                 case (5):
                     // may.
-                    [[fallthrough]]
+                    [[fallthrough]];
 
                 case (7):
                     // july.
-                    [[fallthrough]]
+                    [[fallthrough]];
 
                 case (8):
                     // august.
-                    [[fallthrough]]
+                    [[fallthrough]];
 
                 case (10):
                     // october.
-                    [[fallthrough]]
+                    [[fallthrough]];
                                 
                 case (12): 
                     // december.
@@ -51,22 +52,22 @@ namespace tts {
 
                 case (4):
                     // april.
-                    [[fallthrough]]
+                    [[fallthrough]];
             
                 case (6):
                     // june.
-                    [[fallthrough]]
+                    [[fallthrough]];
 
                 case (9):
                     // september.
-                    [[fallthrough]]
+                    [[fallthrough]];
 
                 case (11):
                     // november.
                     return 30;
                         
                 case (2):
-                    if [[unlikely]] (is_leap())
+                    if (is_leap()) [[unlikely]]
                         return 29;
                     return 28;
 
@@ -78,13 +79,30 @@ namespace tts {
         // predicates!
         [[nodiscard]]
         constexpr bool is_leap() const noexcept {
-            return (year % 4 == 0 && year % 100 != 0)
-                || (year % 400 == 0);
+            return (cmpeq(year % 4, 0) && cmpneq(year % 100, 0))
+                || cmpeq(year % 400, 0);
         }
 
         [[nodiscard]]
-        constexpr is_end_of_month() const noexcept {
-             
+        constexpr bool is_start_of_month() const noexcept {
+            return cmpeq(day, (u16)1);         
+        }
+
+        [[nodiscard]]
+        constexpr bool is_end_of_month() const noexcept {
+            return cmpeq(day, days_in_month());         
+        }
+        
+        [[nodiscard]]
+        constexpr bool is_start_of_year() const noexcept {
+            return (is_start_of_month() &&
+                cmpeq(month, (u16)1));         
+        }
+        
+        [[nodiscard]]
+        constexpr bool is_end_of_year() const noexcept {
+            return (cmpeq(day, (u16)31) && 
+                cmpeq(month, (u16)12));         
         }
     };
 } // namespace tts.
