e7e5574e5665260ce97aec93c95e5648d1fab7a8
jjaemun <jjaemuuu@gmail.com>
2026-03-22 12:34:02 +0100

    civil

--- include/date/gregorian/civil.hpp
+++ include/date/gregorian/civil.hpp
@@ -15,6 +15,76 @@ namespace tts {
     public:
         i32 year;
         u16 month;
-        u16 day;
+        u16 day;a
+
+        // attr!
+        [[nodiscard]]
+        constexpr bool days_in_month() const noexcept {
+            switch (month) {
+                case (1):
+                    // january.
+                    [[fallthrough]]
+
+                case (3):  
+                    // march.
+                    [[fallthrough]]
+
+                case (5):
+                    // may.
+                    [[fallthrough]]
+
+                case (7):
+                    // july.
+                    [[fallthrough]]
+
+                case (8):
+                    // august.
+                    [[fallthrough]]
+
+                case (10):
+                    // october.
+                    [[fallthrough]]
+                                
+                case (12): 
+                    // december.
+                    return 31;
+
+                case (4):
+                    // april.
+                    [[fallthrough]]
+            
+                case (6):
+                    // june.
+                    [[fallthrough]]
+
+                case (9):
+                    // september.
+                    [[fallthrough]]
+
+                case (11):
+                    // november.
+                    return 30;
+                        
+                case (2):
+                    if [[unlikely]] (is_leap())
+                        return 29;
+                    return 28;
+
+                default:
+                    __builtin_unreachable();
+            }
+        }
+
+        // predicates!
+        [[nodiscard]]
+        constexpr bool is_leap() const noexcept {
+            return (year % 4 == 0 && year % 100 != 0)
+                || (year % 400 == 0);
+        }
+
+        [[nodiscard]]
+        constexpr is_end_of_month() const noexcept {
+             
+        }
     };
 } // namespace tts.
