53af637f548a327dd81050870566213cb47bd2ae
jjaemun <jjaemuuu@gmail.com>
2026-03-22 15:50:58 +0100

    prelude

---
+++ include/date/prelude.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+
+#include "constants.hpp"
+#include "conversions.hpp"
+#include "date.hpp"
+#include "errors.hpp"
+#include "exceptions.hpp"
+#include "predicates.hpp"
+#include "validation.hpp"
+
+#include "gregorian/gregorian.hpp"
+#include "ops/ops.hpp"
