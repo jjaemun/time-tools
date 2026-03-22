164f4c90cf5058b75677d16a89fd7e07659ed546
jjaemun <jjaemuuu@gmail.com>
2026-03-22 23:16:05 +0100

    prelude

--- include/date/gregorian/gregorian.hpp
+++ include/date/gregorian/gregorian.hpp
@@ -1,6 +1,11 @@
 #pragma once
 
+
+#include "algorithms.hpp"
+#include "constants.hpp"   
+#include "errors.hpp"
+#include "exceptions.hpp"
+#include "validation.hpp"
 #include "civil.hpp"
-#include "constants.hpp"
 #include "conversions.hpp"
 #include "predicates.hpp"
