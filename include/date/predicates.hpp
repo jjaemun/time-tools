57aa009a53db8bb48ad785ea8e62f82a05d5fbb9
jjaemun <jjaemuuu@gmail.com>
2026-03-22 13:35:09 +0100

    predicates

---
+++ include/date/predicates.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+
+#include "date.hpp"
+
+
+namespace tts {
+    // serial predicates!
+    [[nodiscard]]
+    constexpr bool is_weekend(Date date) noexcept {
+        return date.is_weekend();
+    }
+
+    [[nodiscard]]
+    constexpr bool is_weekday(Date date) noexcept {
+        return date.is_weekday();
+    }
+
+    // gregorian predicates!
+    [[nodiscard]]
+    constexpr bool is_leap(Date date) noexcept {
+        return date.is_leap();
+    }
+        
+    [[nodiscard]]
+    constexpr bool is_start_of_month(Date date) noexcept {
+        return date.is_start_of_month();
+    }
+        
+    [[nodiscard]]
+    constexpr bool is_end_of_month(Date date) noexcept {
+        return date.is_end_of_month();
+    }
+         
+    [[nodiscard]]
+    constexpr bool is_start_of_year(Date date) noexcept {
+        return date.is_start_of_year();
+    }
+ 
+    [[nodiscard]]
+    constexpr bool is_end_of_year(Date date) noexcept {
+        return date.is_end_of_year();
+    }
+} // namespace tts.
