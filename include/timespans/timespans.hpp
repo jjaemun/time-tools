46c8e00564d613b10c82984fdbcdf52cc93c9e79
jjaemun <jjaemuuu@gmail.com>
2026-03-23 21:59:27 +0100

    testing...

--- include/timespans/timespans.hpp
+++ include/timespans/timespans.hpp
@@ -21,25 +21,30 @@ namespace tts {
             : value(value_) {}
 
         // named-constructors!
+        [[nodiscard]]
         static constexpr MilliSeconds from_seconds(i32 seconds) noexcept {
             return MilliSeconds{seconds * i32{1000}};
         }
         
+        [[nodiscard]]
         static constexpr MilliSeconds from_minutes(i32 minutes) noexcept {
             return MilliSeconds{minutes * i32{60} 
                 * i32{1000}};
         }
         
+        [[nodiscard]]
         static constexpr MilliSeconds from_hours(i32 hours) noexcept {
             return MilliSeconds{hours * i32{60} * i32{60} 
                 * i32{1000}};
         }
         
+        [[nodiscard]]
         static constexpr MilliSeconds from_days(i32 days) noexcept {
             return MilliSeconds{days * i32{24} * i32{60} 
                 * i32{60} * i32{1000}};
         }
         
+        [[nodiscard]]
         static constexpr MilliSeconds from_weeks(i32 weeks) noexcept {
             return MilliSeconds{weeks * i32{7} * i32{24} 
                 * i32{60} * i32{60} * i32{1000}};
@@ -63,23 +68,28 @@ namespace tts {
             : value(value_) {}
 
         // named-constructors!
+        [[nodiscard]]
         static constexpr Seconds from_milliseconds(i32 milliseconds) noexcept {
-            return Seconds{seconds / i32{1000}};
+            return Seconds{milliseconds / i32{1000}};
         }
         
+        [[nodiscard]]
         static constexpr Seconds from_minutes(i32 minutes) noexcept {
             return Seconds{minutes * i32{60}};
         }
         
+        [[nodiscard]]
         static constexpr Seconds from_hours(i32 hours) noexcept {
             return Seconds{hours * i32{60} * i32{60}};
         }
         
+        [[nodiscard]]
         static constexpr Seconds from_days(i32 days) noexcept {
             return Seconds{days * i32{24} * i32{60} 
                 * i32{60}};
         }
         
+        [[nodiscard]]
         static constexpr Seconds from_weeks(i32 weeks) noexcept {
             return Seconds{weeks * i32{7} * i32{24} 
                 * i32{60} * i32{60}};
@@ -104,36 +114,31 @@ namespace tts {
             : value(value_) {}
 
         // named-constructors!
+        [[nodiscard]]
         static constexpr Minutes from_milliseconds(i32 milliseconds) noexcept {
-            i32 minutes = milliseconds / (i32{1000} 
-                * i32{60});
-        
-            return Minutes{minutes};
+            return Minutes{milliseconds / (i32{1000} 
+                * i32{60})};
         }
         
+        [[nodiscard]]
         static constexpr Minutes from_seconds(i32 seconds) noexcept {
-            i32 minutes = seconds / i32{60};
-
-            return Minutes{minutes};
+            return Minutes{seconds / i32{60}};
         }
         
+        [[nodiscard]]
         static constexpr Minutes from_hours(i32 hours) noexcept {
-            i32 minutes = hours * i32{60};
-
-            return Minutes{minutes};
+            return Minutes{hours * i32{60}};
         }
         
+        [[nodiscard]]
         static constexpr Minutes from_days(i32 days) noexcept {
-            i32 minutes = hours * i32{24} * i32{60};
-
-            return Minutes{minutes};
+            return Minutes{days * i32{24} * i32{60}};
         }
         
+        [[nodiscard]]
         static constexpr Minutes from_weeks(i32 weeks) noexcept {
-            i32 minutes = weeks * i32{7} * i32{24} 
-                * i32{60};
-
-            return Minutes{minutes};
+            return Minutes{weeks * i32{7} * i32{24} 
+                * i32{60}};
         }
 
         // copy-accessors.
@@ -153,34 +158,38 @@ namespace tts {
         // constructor!
         explicit constexpr Hours(i32 value_) noexcept
             : value(value_) {}
-
+        
         // named-constructors!
-        static constexpr Seconds from_milliseconds(i32 milliseconds) noexcept {
-            return MilliSeconds{seconds / i32{1000}};
+        [[nodiscard]]
+        static constexpr Hours from_milliseconds(i32 milliseconds) noexcept {
+            return Hours{milliseconds / (i32{1000} 
+                * i32{60} * i32{60})};
         }
         
-        static constexpr Seconds from_minutes(i32 minutes) noexcept {
-            return MilliSeconds{minutes * i32{60}};
+        [[nodiscard]]
+        static constexpr Hours from_seconds(i32 seconds) noexcept {
+            return Hours{seconds / (i32{60} 
+                * i32{60})};
+        }
+         
+        [[nodiscard]]
+        static constexpr Hours from_minutes(i32 minutes) noexcept {
+            return Hours{minutes / i32{60}};
+        }       
+       
+        [[nodiscard]]
+        static constexpr Hours from_days(i32 days) noexcept {
+            return Hours{days * i32{24}}; 
         }
         
-        static constexpr Seconds from_hours(i32 hours) noexcept {
-            return Seconds{hours * i32{60} * i32{60}};
-        }
-        
-        static constexpr Seconds from_days(i32 days) noexcept {
-            return Seconds{days * i32{24} * i32{60} 
-                * i32{60}};
-        }
-        
-        static constexpr Seconds from_weeks(i32 weeks) noexcept {
-            return MilliSeconds{weeks * i32{7} * i32{24} 
-                * i32{60} * i32{60}};
+        [[nodiscard]]
+        static constexpr Hours from_weeks(i32 weeks) noexcept {
+            return Hours{weeks * i32{7} * i32{24}};
         }
 
         // copy-accessors.
-  
+    };
 
-};
     class Days final {
         
         /**
@@ -192,8 +201,84 @@ namespace tts {
 
     public:
         // constructor!
-        explicit constexpr Days(i32 days_) : days(days_) {}
+        explicit constexpr Days(i32 value_) noexcept 
+            : value(value_) {}
+        
+        // named-constructors!
+        [[nodiscard]]
+        static constexpr Days from_milliseconds(i32 milliseconds) noexcept {
+            return Days{milliseconds / (i32{1000} 
+                * i32{60} * i32{60} * i32{24})};
+        }
+        
+        [[nodiscard]]
+        static constexpr Days from_seconds(i32 seconds) noexcept {
+            return Days{seconds / (i32{60} 
+                * i32{60} * i32{24})};
+        }
+         
+        [[nodiscard]]
+        static constexpr Days from_minutes(i32 minutes) noexcept {
+            return Days{minutes / (i32{60}
+                * i32{24})};
+        }       
+       
+        [[nodiscard]]
+        static constexpr Days from_hours(i32 hours) noexcept {
+            return Days{days / i32{24}}; 
+        }
+        
+        [[nodiscard]]
+        static constexpr Days from_weeks(i32 weeks) noexcept {
+            return Hours{weeks * i32{7}};
+        }
+
+        // copy-accessors.
     };
 
-    class Weeks {};
+    class Weeks {
+                
+        /**
+         * Weeks timespan abstraction. True weeks.
+        */
+
+    private:
+        i32 value;
+
+    public:
+        // constructor!
+        explicit constexpr Weeks(i32 value_) noexcept 
+            : value(value_) {}
+        
+        // named-constructors!
+        [[nodiscard]]
+        static constexpr Weeks from_milliseconds(i32 milliseconds) noexcept {
+            return Weeks{milliseconds / (i32{1000} * i32{60} 
+                * i32{60} * i32{24} * i32{7})};
+        }
+        
+        [[nodiscard]]
+        static constexpr Weeks from_seconds(i32 seconds) noexcept {
+            return Weeks{seconds / (i32{60} * i32{60} 
+                * i32{24} * i32{7})};
+        }
+         
+        [[nodiscard]]
+        static constexpr Weeks from_minutes(i32 minutes) noexcept {
+            return Weeks{minutes / (i32{60} * i32{24} 
+                * i32{7})};
+        }       
+       
+        [[nodiscard]]
+        static constexpr Weeks from_hours(i32 hours) noexcept {
+            return Weeks{days / (i32{24} * i32{7}}; 
+        }
+        
+        [[nodiscard]]
+        static constexpr Weeks from_days(i32 days) noexcept {
+            return Weeks{days / i32{7}};
+        }
+
+        // copy-accessors.
+    };
 } //namespace tts.
