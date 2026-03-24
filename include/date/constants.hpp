6e7faef7815f416fe9527c5dc95d7d2b492e597b
jjaemun <jjaemuuu@gmail.com>
2026-03-24 18:06:08 +0100

    constants

--- include/date/constants.hpp
+++ include/date/constants.hpp
@@ -16,27 +16,27 @@ namespace tts {
     namespace unix::date {
         // epoch 1970-01-01.
         constexpr i32 OFFSET{0};
-        constexpr i32 MIN{3652};
-        constexpr i32 MAX{84370};
+        constexpr i32 MIN{3'652};
+        constexpr i32 MAX{84'370};
     } // namespace unix::date.
 
     namespace excel::date {
         // epoch 1899-12-30.
-        constexpr i32 OFFSET{25569};
+        constexpr i32 OFFSET{25'569};
         constexpr i32 MIN{OFFSET + unix::date::MIN};
         constexpr i32 MAX{OFFSET + unix::date::MAX};     
     } // namespace excel::date.
     
     namespace murex::date {
         // epoch 1980-01-01.
-        constexpr i32 OFFSET{3652};
+        constexpr i32 OFFSET{3'652};
         constexpr i32 MIN{OFFSET + unix::date::MIN};
         constexpr i32 MAX{OFFSET + unix::date::MAX};     
     } // namespace murex::date.
 
     namespace julian::date {
         // epoch (b.C.) 4714-11-24.
-        constexpr i32 OFFSET{2440588};
+        constexpr i32 OFFSET{2'440'588};
         constexpr i32 MIN{OFFSET + unix::date::MIN};
         constexpr i32 MAX{OFFSET + unix::date::MAX};
     } // namespace julian::date.
