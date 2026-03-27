fe75c38209d50e862af9d1a9f4cb42ca8ac37f72
jjaemun <jjaemuuu@gmail.com>
2026-03-27 23:02:07 +0100

    assign

---
+++ include/tts/date/ops/assign.hpp
@@ -0,0 +1,31 @@
+#pragma once 
+
+
+#include "tts/cmp.hpp"
+#include "tts/date/date.hpp"
+#include "tts/tspans/prelude.hpp"
+
+
+namespace tts {
+    // tspan-add-assign!
+    template <typename T, i64 N>
+    inline Date &operator+=(Date &date, TimeSpan<T, N> tspan) {
+        const auto advanced = date.to_unix_serial() + tspan.to_days();
+        if (!cmplt(advanced, unix::date::MAX))
+            throw DateError(err::date::overflow);
+
+        date.as_mut_unix_serial() = advanced;
+        return date;
+    }
+
+    // tspan-sub-assign!
+    template <typename T, i64 N>
+    inline Date &operator-=(Date &date, TimeSpan<T, N> tspan) {
+        const auto regressed = date.to_unix_serial() - tspan.to_days();
+        if (!cmpgt(regressed, unix::date::MIN))
+            throw DateError(err::date::overflow);
+    
+        date.as_mut_unix_serial() = regressed;
+        return date;
+    }
+} // namespace tts.
