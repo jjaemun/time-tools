f4cd6255ed17331ca8250c7adc474a7f082bed1e
jjaemun <jjaemuuu@gmail.com>
2026-03-27 22:34:27 +0100

    ops & prelude

---
+++ include/tts/tspans/ops/ops.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+
+#include "tts/tspans/ops/binary.hpp"
+#include "tts/tspans/ops/unary.hpp"
+#include "tts/tspans/ops/scalar.hpp"
+#include "tts/tspans/ops/ord.hpp"
+#include "tts/tspans/ops/assign.hpp"
---
+++ include/tts/tspans/prelude.hpp
@@ -0,0 +1,7 @@
+#pragma once 
+
+
+#include "tts/tspans/constants.hpp"
+#include "tts/tspans/tspans.hpp"
+#include "tts/tspans/hash.hpp"
+#include "tts/tspans/ops/ops.hpp"
