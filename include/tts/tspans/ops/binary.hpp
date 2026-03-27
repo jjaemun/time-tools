47cb03e96c877e982dfb0a9568c995582ea60d87
jjaemun <jjaemuuu@gmail.com>
2026-03-27 17:33:44 +0100

    binary

---
+++ include/tts/tspans/ops/binary.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+
+#include "tts/tspans/tspans.hpp"
+
+
+namespace tts {
+    
+    /**
+     * Binary operations promote mismatching timespan scales to the coarsest
+     * of the two operands. This enforces a general convention while allowing
+     * finer scale return types when using inline explicit conversions.
+    */
+
+    template <typename T, i32 N, typename U, i32 M>
+    [[noexcept]]
+    constexpr auto operator+(TimeSpan<T, N> lhs, TimeSpan<U, M> rhs) noexcept {
+ 
+        /**
+         * Notice that actual return type depends on branching, and so
+         * using auto fully generalises. It is the coarsest by default.
+        */
+
+        if constexpr (N > M)
+            return TimeSpan<T, N>{lhs.count() + rhs.to<T, N>().count()};
+        else
+            return TimeSpan<U, M>{lhs.to<U, M>().count() + rhs.count()};
+    }
+
+    template <typename T, i32 N, typename U, i32 M>
+    [[noexcept]]
+    constexpr auto operator-(TimeSpan<T, N> lhs, TimeSpan<U, M> rhs) noexcept {
+  
+        /**
+         * Notice that actual return type depends on branching, and so
+         * using auto fully generalises. It is the coarsest by default.
+        */
+
+       if constexpr (N > M)
+            return TimeSpan<T, N>{lhs.count() - rhs.to<T, N>().count()};
+        else
+            return TimeSpan<U, M>{lhs.to<U, M>().count() - rhs.count()};
+    }
+} // namespace tts.
