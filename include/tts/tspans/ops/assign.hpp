ad2ef869832cc514ace9d8c6c1f64a53447e7e1a
jjaemun <jjaemuuu@gmail.com>
2026-03-27 18:34:28 +0100

    assign

---
+++ include/tts/tspans/ops/assign.hpp
@@ -0,0 +1,39 @@
+#pragma once 
+
+
+#include "tts/tspans/tspans.hpp"
+#include "tts/tspans/ops/binary.hpp"
+#include "tts/tspans/ops/scalar.hpp"
+
+
+namespace tts {
+    // add-assign!
+    template <typename T, i32 N, typename U, i32 M>
+    constexpr TimeSpan<T, N> &operator+=(TimeSpan<T, N> &lhs, TimeSpan<U, M> rhs) noexcept {
+        lhs.as_mut_count() += rhs.to<T, N>().count();
+        return lhs;
+    }
+   
+    // sub-assign!
+    template <typename T, i32 N, typename U, i32 M>
+    constexpr TimeSpan<T, N> &operator-=(TimeSpan<T, N> &lhs, TimeSpan<U, M> rhs) noexcept {
+        lhs.as_mut_count() -= rhs.to<T, N>().count();
+        return lhs;
+    }
+
+    // scalar mul-assign!
+    template <typename T, i32 N, typename U>
+    constexpr TimeSpan<T, N> &operator*=(TimeSpan<T, N> &tspan, U scalar) noexcept {
+        tspan.as_mut_count() *= scalar;
+        return tspan;
+    }
+
+    // scalar div-assign!
+    template <typename T, i32 N, typename U>
+    constexpr TimeSpan<T, N> &operator/=(TimeSpan<T, N> &tspan, U scalar) noexcept {
+        tspan.as_mut_count() /= scalar;
+        return tspan;
+    }
+} // namespace tts.
+
+
