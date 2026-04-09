8e9721cc5fa8cc7a379c286947b550a3d53a51c9
jjaemun <jjaemuuu@gmail.com>
2026-04-10 00:53:18 +0200

    prelude

---
+++ include/tts/months/prelude.hpp
@@ -0,0 +1,7 @@
+#pragma once 
+
+
+#include "tts/months/constants.hpp"
+#include "tts/months/months.hpp"
+#include "tts/months/hash.hpp"
+#include "tts/months/ops/ops.hpp"
---
+++ include/tts/years/prelude.hpp
@@ -0,0 +1,7 @@
+#pragma once 
+
+
+#include "tts/years/constants.hpp"
+#include "tts/years/years.hpp"
+#include "tts/years/hash.hpp"
+#include "tts/years/ops/ops.hpp"
